903cc4016608f0efd23be0cd3bd458b89247dbd8
Jacob Delgado <arcjcb11@gmail.com>
2022-10-11 01:11:52 -0700

    refactored and finished .env
    refactored Firebase.js from lowercase and moved all application variables to .env

--- src/firebase.js
+++ src/firebase.js
@@ -1,30 +1,73 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import { getFirestore } from "firebase/firestore";
-import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, onAuthStateChanged } from 'firebase/auth'
-
-
-
+import {
+    GoogleAuthProvider,
+    getAuth,
+    createUserWithEmailAndPassword,
+    signInWithPopup,
+    signInWithEmailAndPassword,
+    signOut,
+} from "firebase/auth";
+import {
+    getFirestore,
+    query,
+    getDocs,
+    collection,
+    where,
+    addDoc,
+} from "firebase/firestore";
 const firebaseConfig = {
     apiKey: process.env.REACT_APP_FIREBASE_Key,
-    authDomain: "bretry-flex.firebaseapp.com",
-    projectId: "bretry-flex",
-    storageBucket: "bretry-flex.appspot.com",
-    messagingSenderId: "654836403109",
-    appId: "1:654836403109:web:54b8ffe479dea098e4b64c",
-    measurementId: "G-GZ2K32VY9F"
+    authDomain: process.env.REACT_APP_FIREBASE_authDomain,
+    projectId: process.env.REACT_APP_FIREBASE_projectId,
+    storageBucket: process.env.REACT_APP_FIREBASE_storageBucket,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_messagingSenderId,
+    appId: process.env.REACT_APP_FIREBASE_appId,
+    measurementId: process.env.REACT_APP_FIREBASE_measurementId
+  };
+
+  const app = initializeApp(firebaseConfig);
+  const auth = getAuth(app);
+  const db = getFirestore(app);
+  const googleProvider = new GoogleAuthProvider();
+
+  const signInWithGoogle = async () => {
+    try {
+        const res = await signInWithPopup(auth, googleProvider);
+        const user = res.user;
+        const q = query(collection(db, "users"), where("uid", "==", user.uid));
+        const docs = await getDocs(q);
+        if (docs.docs.length === 0) {
+          await addDoc(collection(db, "users"), {
+            uid: user.uid,
+            name: user.displayName,
+            authProvider: "google",
+            email: user.email,
+          });
+        }
+      } catch (err) {
+        console.error(err);
+        alert(err.message);
+      }
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
-export const auth = getAuth(app);
-export const methods = {
+const logInWithEmailAndPassword = async (email, password) => {
+    try {
+      await signInWithEmailAndPassword(auth, email, password);
+    } catch (err) {
+      console.error(err);
+      alert(err.message);
+    }
+};
+
+  const logout = () => {
+    signOut(auth);
+};
+
+export {
+    auth,
+    db,
+    signInWithGoogle,
     createUserWithEmailAndPassword,
-    signInWithEmailAndPassword,
-    onAuthStateChanged,
-    signOut
-}
-export default app;
-const analytics = getAnalytics(app);
+    logInWithEmailAndPassword,
+    logout,
+  };
