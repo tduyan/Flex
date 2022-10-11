a62dbf70da41c056dd0fac24de4a2ecdcd00ebff
Jacob Delgado <arcjcb11@gmail.com>
2022-10-10 23:32:14 -0700

    fixed react import
    required for jsx components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 //import React, { useState } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
+import React, { Component } from 'react'
 import Login from './Login';
 //import LandingPage from './LandingPage';
 
