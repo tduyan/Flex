89c0872b0ff38c621d758bbbff309a142198adfb
cantdrinkmilk <53961561+cantdrinkmilk@users.noreply.github.com>
2022-10-16 23:37:00 -0700

    resetUI
    ResetUI

--- src/components/Reset.js
+++ src/components/Reset.js
@@ -2,7 +2,8 @@ import { sendPasswordResetEmail } from 'firebase/auth';
 import React , { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import {auth, resetPassword} from '../firebase';
-import {AuthContext} from '../AuthContext';
+import './Reset.css'
+
 
 
 
@@ -12,18 +13,19 @@ function Reset(){
     const [email, setEmail] = useState('')
     
   
-    const resetPassword = () => {
-      if(email){
-        sendPasswordResetEmail(email).then(() => {
-          email.current.value= "";
-      });
+    // const resetPassword = () => {
+    //   if(email){
+    //     sendPasswordResetEmail(email).then(() => {
+    //       email.current.value= "";
+    //   });
 
-      };
+    // }
 
     return(
 
         <div className="reset">
           <div className="reset__container">
+            <h2 className='reset__header'>Enter Email</h2>
             <input
               type="text"
               className="reset__textBox"
@@ -32,15 +34,16 @@ function Reset(){
               placeHolder="Email"
             />
             <button
-            className="reset__btn"
-            onClick={resetPassword}> Send Link</button>
-            
+            className="reset__btn"> SEND RESET EMAIL
+            {/* // onClick={resetPassword}> Send Link</button> */}
+            </button>
+          
           </div>
         </div>
     );
 
 }
 
-}
+
 
 export default Reset;
