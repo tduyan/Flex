bc0a731741d0fcf6dd08c0e4034636ecdec41cc0
Jacob Delgado <arcjcb11@gmail.com>
2022-11-28 00:18:21 -0800

    fixed navigation on useraccount page

--- src/UserAccount.js
+++ src/UserAccount.js
@@ -8,6 +8,7 @@ import { Form,Button } from 'react-bootstrap';
 import {logout} from "./firebase.js";
 import { getAuth} from "firebase/auth";
 import { useAuthState } from 'react-firebase-hooks/auth';
+import Navigation from './Navigation'
 
 const UserAccount = () => {
     const newEmail = useRef();
@@ -42,40 +43,7 @@ const UserAccount = () => {
 
     return(
         <div>
-            <Navbar bg="dark" expand="lg">
-              <Navbar.Toggle aria-controls="basic-navbar-nav" />
-              <Navbar.Collapse id="basic-navbar-nav">
-                  <Nav>
-                      <NavLink className="d-inline p-2 bg-dark text-white" to="/Home">
-                          Home
-                      </NavLink>
-                      <NavLink className="d-inline p-2 bg-dark text-white" to="/TVShows">
-                          TV Shows
-                      </NavLink>
-                      <NavLink className="d-inline p-2 bg-dark text-white" to="/Movies">
-                          Movies
-                      </NavLink>
-                      <NavLink className="d-inline p-2 bg-dark text-white" to="/ActWiki">
-                          ActWiki
-                      </NavLink>
-                  </Nav>
-              </Navbar.Collapse>
-              <Form className="d-flex">
-                  <Form.Control
-                      type="search"
-                      placeholder="Search"
-                      className="me-2"
-                      aria-label="Search"
-                  />
-                  <Button variant="outline-success">Search</Button>
-              </Form>
-              <NavLink className="d-inline p-2 bg-dark text-white ml-auto" to="/UserAccount">
-                  Account
-              </NavLink>
-              <button className="d-inline p-2 bg-dark text-white ml-auto" onClick={logout}>
-                  Log Out
-              </button>
-          </Navbar>
+          <Navigation />
           <h1 className='header'>{user.email}</h1>
           <div className='UserAccount__container'>   
               <button className="AdminButton"><Link to="/AdminConsole">AdminConsole</Link></button>
