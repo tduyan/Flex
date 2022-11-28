f4fdbfdaa97a6889f992c5916996e948695d159e
Jacob Delgado <arcjcb11@gmail.com>
2022-11-27 23:02:13 -0800

    cleaned user account

--- src/UserAccount.js
+++ src/UserAccount.js
@@ -6,7 +6,7 @@ import { NavLink } from 'react-router-dom'
 import { Navbar, Nav } from 'react-bootstrap'
 import { Form,Button } from 'react-bootstrap';
 import {logout} from "./firebase.js";
-import { getAuth, updatePassword} from "firebase/auth";
+import { getAuth} from "firebase/auth";
 import { useAuthState } from 'react-firebase-hooks/auth';
 
 const UserAccount = () => {
@@ -15,7 +15,7 @@ const UserAccount = () => {
     const confirmPsswd = useRef();
     const auth = getAuth();
     const [user] = useAuthState(auth);
-    const {ogoutUser, updateUserEmail, updateUserPassword} = useUserContext("");
+    const {logoutUser, updateUserEmail, updateUserPassword} = useUserContext("");
 
     // const handleSubmit = async (e) =>{
     //     e.preventDefault();
@@ -34,9 +34,7 @@ const UserAccount = () => {
       const psswd2 = confirmPsswd.current.value;
 
       if(psswd === psswd2){
-        updateUserPassword(psswd).then(() => {
-          newPsswd.current.value= "";
-        });
+        updateUserPassword(newPsswd.current.value);
       }else{
         alert("Passwords do not match");
       };
@@ -44,72 +42,73 @@ const UserAccount = () => {
 
     return(
         <div>
-          <Navbar bg="dark" expand="lg">
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-                <Nav>
-                    <NavLink className="d-inline p-2 bg-dark text-white" to="/Home">
-                        Home
-                    </NavLink>
-                    <NavLink className="d-inline p-2 bg-dark text-white" to="/TVShows">
-                        TV Shows
-                    </NavLink>
-                    <NavLink className="d-inline p-2 bg-dark text-white" to="/Movies">
-                        Movies
-                    </NavLink>
-                    <NavLink className="d-inline p-2 bg-dark text-white" to="/ActWiki">
-                        ActWiki
-                    </NavLink>
-                </Nav>
-            </Navbar.Collapse>
-            <Form className="d-flex">
-                <Form.Control
-                    type="search"
-                    placeholder="Search"
-                    className="me-2"
-                    aria-label="Search"
-                />
-                <Button variant="outline-success">Search</Button>
-            </Form>
-            <NavLink className="d-inline p-2 bg-dark text-white ml-auto" to="/UserAccount">
-                Account
-            </NavLink>
-            <button className="d-inline p-2 bg-dark text-white ml-auto" onClick={logout}>
-                Log Out
-            </button>
-        </Navbar>
-        <div className='UserAccount__container'>
-            <div>
-            <h1>Email: {user.email}</h1>
-            <button className="AdminButton"><Link to="/AdminConsole">AdminConsole</Link></button>
-            </div>
-        </div>
-        <div className="updateEmail">
+            <Navbar bg="dark" expand="lg">
+              <Navbar.Toggle aria-controls="basic-navbar-nav" />
+              <Navbar.Collapse id="basic-navbar-nav">
+                  <Nav>
+                      <NavLink className="d-inline p-2 bg-dark text-white" to="/Home">
+                          Home
+                      </NavLink>
+                      <NavLink className="d-inline p-2 bg-dark text-white" to="/TVShows">
+                          TV Shows
+                      </NavLink>
+                      <NavLink className="d-inline p-2 bg-dark text-white" to="/Movies">
+                          Movies
+                      </NavLink>
+                      <NavLink className="d-inline p-2 bg-dark text-white" to="/ActWiki">
+                          ActWiki
+                      </NavLink>
+                  </Nav>
+              </Navbar.Collapse>
+              <Form className="d-flex">
+                  <Form.Control
+                      type="search"
+                      placeholder="Search"
+                      className="me-2"
+                      aria-label="Search"
+                  />
+                  <Button variant="outline-success">Search</Button>
+              </Form>
+              <NavLink className="d-inline p-2 bg-dark text-white ml-auto" to="/UserAccount">
+                  Account
+              </NavLink>
+              <button className="d-inline p-2 bg-dark text-white ml-auto" onClick={logout}>
+                  Log Out
+              </button>
+          </Navbar>
+          <h1 className='header'>{user.email}</h1>
+          <div className='UserAccount__container'>   
+              <button className="AdminButton"><Link to="/AdminConsole">AdminConsole</Link></button>
+          </div>
+          <div className="updateEmail">
+              <input  
+              type="text"
+              ref={newEmail}
+              placeholder="Enter new email address..."
+              />
+          </div>
+          <div className='updatePass'>
+          <button className="UserAccountpsswd__btn"
+              onClick={updateEmail}><Link to="/">Update Email</Link></button>
+          </div>
+          <div className='updatePass'>
+              <input  
+              type="text"
+              ref={newPsswd}
+              placeholder="Enter new password"
+              />
+          </div> 
+          <div className='updatePass'>
             <input  
             type="text"
-            className="UserAccount__textBox"
-            ref={newEmail}
-            placeholder="Enter new email address..."
-            />
-            <button 
-            onClick={updateEmail}><Link to="/">Update Email</Link></button>
-            <div>
-            <input  
-            type="password"
-            className="UserAccount__textBox"
-            ref={newPsswd}
-            placeholder="Enter new password"
-            />
-            <input  
-            type="password"
-            className="UserAccount__textBox"
             ref={confirmPsswd}
             placeholder="Confirm new password"
             />
-            <button className="UserAccountpsswd__btn"
-            onClick={updatePassword}><Link to="/">Update Password</Link></button>
-            </div>
-        </div>
+          </div>
+          <div className='updatePass'>
+              <button className="UserAccountpsswd__btn"
+              onClick={updatePassword}><Link to="/">Update Password</Link></button>
+          </div>
         </div>
     );
 }
